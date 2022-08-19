57805684a27cae3345b2da14e49d87f5cf7abe05
Nick Cao <nickcao@nichi.co>
2022-08-19 20:18:07 +0800

    add sanity checks

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,15 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn knot_ctl() {
+        unsafe {
+            let ctl = super::knot_ctl_alloc();
+            super::knot_ctl_set_timeout(ctl, 1000);
+            super::knot_ctl_free(ctl);
+        }
+    }
+}

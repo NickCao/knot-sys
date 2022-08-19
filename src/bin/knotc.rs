8b654ad37480deec638395bc26fbd89c8a608190
Nick Cao <nickcao@nichi.co>
2022-08-19 23:00:16 +0800

    structured data

--- src/bin/knotc.rs
+++ src/bin/knotc.rs
@@ -1,6 +1,7 @@
 #![feature(concat_idents)]
 #![allow(non_upper_case_globals)]
 use knot_sys::*;
+use std::collections::HashMap;
 use std::ffi::{CStr, CString};
 
 fn main() {
@@ -16,8 +17,6 @@ fn main() {
         let mut data: knot_ctl_data_t = std::mem::zeroed();
         let cmd = CString::new("zone-status").unwrap();
         data[knot_ctl_idx_t_KNOT_CTL_IDX_CMD as usize] = cmd.as_ptr();
-        let flags = CString::new("F").unwrap();
-        data[knot_ctl_idx_t_KNOT_CTL_IDX_FLAGS as usize] = flags.as_ptr();
         let code = knot_ctl_send(
             ctx,
             knot_ctl_type_t_KNOT_CTL_TYPE_DATA,
@@ -36,6 +35,8 @@ fn main() {
             eprintln!("2: {:?}", CStr::from_ptr(knot_strerror(code)));
         }
 
+        let mut metrics = HashMap::<String, HashMap<String, String>>::new();
+
         loop {
             let mut data: knot_ctl_data_t = std::mem::zeroed();
             let mut data_type: knot_ctl_type_t = std::mem::zeroed();
@@ -49,30 +50,26 @@ fn main() {
             }
             match data_type {
                 knot_ctl_type_t_KNOT_CTL_TYPE_BLOCK => break,
-                knot_ctl_type_t_KNOT_CTL_TYPE_EXTRA => {
-                    let r#type = CStr::from_ptr(data[knot_ctl_idx_t_KNOT_CTL_IDX_TYPE as usize])
-                        .to_str()
-                        .unwrap();
-                    let data = CStr::from_ptr(data[knot_ctl_idx_t_KNOT_CTL_IDX_DATA as usize])
-                        .to_str()
-                        .unwrap();
-                    print!(" | {type}: {data}");
-                }
-                knot_ctl_type_t_KNOT_CTL_TYPE_DATA => {
-                    let zone = CStr::from_ptr(data[knot_ctl_idx_t_KNOT_CTL_IDX_ZONE as usize])
-                        .to_str()
-                        .unwrap();
-                    let r#type = CStr::from_ptr(data[knot_ctl_idx_t_KNOT_CTL_IDX_TYPE as usize])
-                        .to_str()
-                        .unwrap();
-                    let data = CStr::from_ptr(data[knot_ctl_idx_t_KNOT_CTL_IDX_DATA as usize])
-                        .to_str()
-                        .unwrap();
-                    print!("\n[{zone}] {type}: {data}");
+                knot_ctl_type_t_KNOT_CTL_TYPE_DATA | knot_ctl_type_t_KNOT_CTL_TYPE_EXTRA => {
+                    let zone = data[knot_ctl_idx_t_KNOT_CTL_IDX_ZONE as usize];
+                    let label = data[knot_ctl_idx_t_KNOT_CTL_IDX_TYPE as usize];
+                    let value = data[knot_ctl_idx_t_KNOT_CTL_IDX_DATA as usize];
+                    let zone = CStr::from_ptr(zone).to_str().unwrap().to_owned();
+                    let label = CStr::from_ptr(label).to_str().unwrap().to_owned();
+                    let value = CStr::from_ptr(value).to_str().unwrap().to_owned();
+                    if let Some(sub) = metrics.get_mut(&zone) {
+                        sub.insert(label, value);
+                    } else {
+                        let mut sub = HashMap::new();
+                        sub.insert(label, value);
+                        metrics.insert(zone, sub);
+                    };
                 }
                 _ => unimplemented!(),
             }
         }
+
+        println!("{:?}", metrics);
 
         knot_ctl_close(ctx);
         knot_ctl_free(ctx);

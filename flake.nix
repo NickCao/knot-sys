4fbd8ea0bcb2940ae9f06734189396c6ca94be7f
Nick Cao <nickcao@nichi.co>
2022-08-22 22:18:07 +0800

    build package

--- flake.nix
+++ flake.nix
@@ -1,24 +1,25 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NickCao/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let pkgs = import nixpkgs { inherit system; }; in
-      with pkgs; rec {
-        devShell = mkShell {
-          nativeBuildInputs = with rustPlatform; [
-            bindgenHook
-            rust.cargo
-            rust.rustc
-            rust-analyzer
-            rustfmt
-            clippy
-          ];
-          buildInputs = [
-            knot-dns
-          ];
-        };
-      });
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs { inherit system; }; in
+        with pkgs; rec{
+          devShells.default = mkShell {
+            inputsFrom = [ packages.default ];
+          };
+          packages.default = rustPlatform.buildRustPackage {
+            name = "knot-sys";
+            src = self;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+            nativeBuildInputs = [ rustPlatform.bindgenHook ];
+            buildInputs = [ knot-dns ];
+          };
+        }
+      );
 }
